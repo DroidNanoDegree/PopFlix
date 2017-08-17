47963d5f2f0874a3f9d91832b9cae9a39e482f49
DroidNanoDegree <srikanthbm@gmail.com>
2017-08-16 17:15:47 -0700

    Code clean up.
    - ran “optimized imports” and “reformat code” from “code” menu option
    in Android Studio.

--- app/src/main/java/com/sriky/popflix/settings/SettingsFragment.java
+++ app/src/main/java/com/sriky/popflix/settings/SettingsFragment.java
@@ -1,18 +1,13 @@
 package com.sriky.popflix.settings;
 
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.sriky.popflix.R;
 
