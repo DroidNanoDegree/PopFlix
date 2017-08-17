5f562c1bd2f595034558fefefd2e567af9aafafe
DroidNanoDegree <srikanthbm@gmail.com>
2017-08-16 17:14:08 -0700

    Code clean-up, reformatting etc.
    - ran “optimized imports” and “reformat code” from “code” menu option
    in Android Studio.

--- app/src/main/java/com/sriky/popflix/settings/SettingsActivity.java
+++ app/src/main/java/com/sriky/popflix/settings/SettingsActivity.java
@@ -1,10 +1,8 @@
 package com.sriky.popflix.settings;
 
-import android.support.v4.app.NavUtils;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.sriky.popflix.R;
