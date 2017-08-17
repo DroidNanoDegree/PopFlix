44ceb16bba44c567cae03a6f890a0663b0b00471
DroidNanoDegree <srikanthbm@gmail.com>
2017-08-16 17:35:37 -0700

    Code clean-up
    - ran “optimized imports” and “reformat code” from “code” menu option
    in Android Studio.

--- app/src/main/java/com/sriky/popflix/MovieData.java
+++ app/src/main/java/com/sriky/popflix/MovieData.java
@@ -4,7 +4,6 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
- * Created by sriky on 8/11/17.
  * Represents movie information gathered from TMDB API.
  */
 
@@ -27,44 +26,67 @@ public class MovieData implements Parcelable {
     //movie release date
     private String mReleaseDate;
 
-    public MovieData(){
+    public MovieData() {
     }
 
-    public void setPosterPath(String posterPath) { mPosterPath = posterPath; }
+    public void setPosterPath(String posterPath) {
+        mPosterPath = posterPath;
+    }
 
-    public void setOverview(String overview) { mOverview = overview; }
+    public void setOverview(String overview) {
+        mOverview = overview;
+    }
 
-    public void setVoteAverage(String voteAverage) { mVoteAverage = voteAverage; }
+    public void setVoteAverage(String voteAverage) {
+        mVoteAverage = voteAverage;
+    }
 
-    public void setMovieID(String movieID) { mMovieID = movieID; }
+    public void setMovieID(String movieID) {
+        mMovieID = movieID;
+    }
 
-    public void setTitle(String title) { mTitle = title; }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 
-    public void setReleaseDate(String releaseDate) { mReleaseDate = releaseDate; }
+    public void setReleaseDate(String releaseDate) {
+        mReleaseDate = releaseDate;
+    }
 
-    public String getPosterPath(){
+    public String getPosterPath() {
         return mPosterPath;
     }
 
-    public String getOverview(){
+    public String getOverview() {
         return mOverview;
     }
 
-    public String getVoteAverage(){
+    public String getVoteAverage() {
         return mVoteAverage;
     }
 
-    public String getTitle() { return mTitle; }
+    public String getTitle() {
+        return mTitle;
+    }
 
-    public String getMovieID(){
+    public String getMovieID() {
         return mMovieID;
     }
 
-    public String getReleaseDate() { return mReleaseDate; }
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
 
-    public String toString() { return mPosterPath + "--" + mMovieID + "--" + mOverview + "--"
-            + mVoteAverage + "--" + mTitle + "--" + mReleaseDate; }
+    public String toString() {
+        return mPosterPath + "--" + mMovieID + "--" + mOverview + "--"
+                + mVoteAverage + "--" + mTitle + "--" + mReleaseDate;
+    }
 
+    /**
+     * Helper to generate MovieData object from savedInstanceState.
+     *
+     * @param in parcel from the savedInstanceState.
+     */
     protected MovieData(Parcel in) {
         mPosterPath = in.readString();
         mMovieID = in.readString();
